f5e4d7dc17eb892429c87b56bb2421005d89b734
hajda <hajda@digitus.itk.ppke.hu>
2017-04-03 23:00:13 +0200

    [noartf] - Stuff - see list below
    - smoothScroll directive and controller are finished and invoked in nav
    - Navigation is in place and working with smoothScroll.
    - Sticky titles are almost there, but buggy.
    - pages directive
      - template layout is improved (using div elements instead of tag directive invocation)
      - data is extracted to a temporary service
      - Added more dummy pages to pages template
    - Data is provided by a temporary service, while entities are not created
    - News directive is created (ng-repeats over articles in the news page data)
    - home.html invokes pages directive by attribute invocation in a div instead of tag invocation
    - testSections state defines view with different name (content@ already exists)
    - home.json en is added more dummy data
    - index.html:
      - body element has id
      - nav directive is viewed instead of JHipster navBar
      - content is in container
    - Gallery
      - Gellery
      - Album
      - Media Item
    
    Change-Id: I5fa185357f0792b2ec7c0cf48e58b0a5c43057bc

---
+++ src/main/webapp/app/components/credits/credits.controller.js
@@ -0,0 +1,22 @@
+(function creditsControllerDefinition() {
+    'use strict';
+
+    angular.module('alcApp')
+        .controller('AlcCreditsController', creditsController);
+
+    creditsController.$inject = ['$scope'];
+
+    function creditsController($scope) {
+        var creditsCtrl = this;
+
+        /* private */
+
+        /* public */
+
+        /*      API */
+
+        /*      public function definitions */
+
+        /* implementation details */
+    }
+})();
--- src/main/webapp/app/components/nav/nav.controller.js
+++ src/main/webapp/app/components/nav/nav.controller.js
@@ -8,9 +8,44 @@
         .module('alcApp')
         .controller('AlcNavController', navController);
 
-    navController.$inject = ['$scope', '$window', '$location', 'AlclSmoothScroll'];
+    navController.$inject = ['$scope', '$state', '$timeout',  'AlcTemporaryContentService'];
 
-    function navController($scope, $window, $location, AlcSmoothScroll) {
+    function navController($scope, $state, $timeout, Content) {
+        /* init */
+
+        /** initialize menu */
+        $scope.menu = Content.getMenuItems();
+        $scope.menu.mobileHide = true;
+
+        /* API */
+
+        $scope.onMenuItemClick = onMenuItemClick;
+        $scope.toggleMenu = toggleMenu;
+
+        /* public */
+
+        function onMenuItemClick($event, pageName) {
+            /**
+             * TODO | this i just a quick fix.
+             * TODO | - when on home state, don't go anywhere, JUST SCROLL.
+             * TODO | - when on a single page state (like /#/alc-gallery),
+             * TODO |   then DO NOT SCROLL, go to the "pageName" page's single page state
+             */
+            $state.go('home');
+            $timeout(goToAlcPage, 1);
+
+            function goToAlcPage() {
+                $scope.scrollTo(pageName);
+                $scope.menu.mobileHide = true;
+            }
+        };
+
+        function toggleMenu() {
+            $scope.menu.mobileHide = !$scope.menu.mobileHide;
+        };
 
     }
 })();
+
+
+
