6b308e4eada6ca6e6af4652c89fe89f3a3a23e6f
hajda <hajda@digitus.itk.ppke.hu>
2017-04-03 23:00:13 +0200

    [noartf] - Stuff - see list below
    - smoothScroll directive and controller are finished and invoked in nav
    - Navigation is in place and working with smoothScroll.
    - Sticky titles are almost there, but buggy.
    - pages directive
      - template layout is improved (using div elements instead of tag directive invocation)
      - data is extracted to a temporary service
      - Added more dummy pages to pages template
    - Data is provided by a temporary service, while entities are not created
    - News directive is created (ng-repeats over articles in the news page data)
    - home.html invokes pages directive by attribute invocation in a div instead of tag invocation
    - testSections state defines view with different name (content@ already exists)
    - home.json en is added more dummy data
    - index.html:
      - body element has id
      - nav directive is viewed instead of JHipster navBar
      - content is in container
    - Gallery
      - Gellery
      - Album
      - Media Item
    
    Change-Id: I5fa185357f0792b2ec7c0cf48e58b0a5c43057bc

---
+++ src/main/webapp/app/components/deviceMetrics/deviceMetrics.service.js
@@ -0,0 +1,13 @@
+(function deviceMetricsServiceDefinition() {
+    'use strict';
+
+    angular.module('alcApp')
+        .factory('AlcDeviceMetrics', deviceMetricsService);
+
+    deviceMetricsService.$inject = [];
+
+    function deviceMetricsService() {
+        return {
+        };
+    }
+})();
--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -27,6 +27,7 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/messageformat/messageformat.js',
             'src/main/webapp/bower_components/json3/lib/json3.js',
             'src/main/webapp/bower_components/angular/angular.js',
+            'src/main/webapp/bower_components/angular-touch/angular-touch.js',
             'src/main/webapp/bower_components/angular-aria/angular-aria.js',
             'src/main/webapp/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
             'src/main/webapp/bower_components/angular-cache-buster/angular-cache-buster.js',
