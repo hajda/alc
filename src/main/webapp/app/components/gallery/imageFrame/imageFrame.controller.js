965650985231e5b1f68db46f5eacef89144a2660
hajda <hajda@digitus.itk.ppke.hu>
2017-04-03 23:00:13 +0200

    [noartf] - Stuff - see list below
    - smoothScroll directive and controller are finished and invoked in nav
    - Navigation is in place and working with smoothScroll.
    - Sticky titles are almost there, but buggy.
    - pages directive
      - template layout is improved (using div elements instead of tag directive invocation)
      - data is extracted to a temporary service
      - Added more dummy pages to pages template
    - Data is provided by a temporary service, while entities are not created
    - News directive is created (ng-repeats over articles in the news page data)
    - home.html invokes pages directive by attribute invocation in a div instead of tag invocation
    - testSections state defines view with different name (content@ already exists)
    - home.json en is added more dummy data
    - index.html:
      - body element has id
      - nav directive is viewed instead of JHipster navBar
      - content is in container
    - Gallery
      - Gellery
      - Album
      - Media Item
    
    Change-Id: I5fa185357f0792b2ec7c0cf48e58b0a5c43057bc

---
+++ src/main/webapp/app/components/gallery/imageFrame/imageFrame.controller.js
@@ -0,0 +1,23 @@
+(function imageFrameControllerDefinition() {
+    'use strict';
+
+    angular
+        .module('alcApp')
+        .controller('AlcImageFrameController', imageFrameController);
+
+    imageFrameController.$inject = ['$scope'];
+
+    function imageFrameController($scope) {
+        var imageFrameCtrl = this;
+
+        /* private */
+
+        /* public */
+
+        /*      API */
+
+        /*      public function definitions */
+
+        /* implementation details */
+    }
+})();
