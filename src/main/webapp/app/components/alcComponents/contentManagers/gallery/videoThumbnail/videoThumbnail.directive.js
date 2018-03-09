5f46eeaca118483b3d9180862fcfdeb88daf741f
David Hajba <david.hajba@outlook.com>
2018-03-09 20:52:21 +0100

    [noartf] - fix a lot of things
    - videos overlaping everything fixed
    - videos layoyt improved
    - header leaves a gap on the top of the page in landscape orientation fixed
    - horrible page title color changed, title changes color based on if it is sticked or not
    - gulp version updated
    - tmp crapy backend mock ignored
    
    Change-Id: I1b4f16bb17d647d52712e736079fa33a9138f6df

--- src/main/webapp/app/components/alcComponents/contentManagers/gallery/videoThumbnail/videoThumbnail.directive.js
+++ src/main/webapp/app/components/alcComponents/contentManagers/gallery/videoThumbnail/videoThumbnail.directive.js
@@ -3,9 +3,9 @@
     angular.module('alcApp')
         .directive('alcVideoThumbnail', videoThumbnailDirective);
 
-    videoThumbnailDirective.$inject = ['$sce'];
+    videoThumbnailDirective.$inject = ['$sce', '$window', 'AlcImageFrame'];
 
-    function videoThumbnailDirective($sce) {
+    function videoThumbnailDirective($sce, $window, ImageFrame) {
         return {
             scope: {},
             restrict: 'AE',
@@ -16,13 +16,26 @@
                 $scope.playing = false;
                 $scope.videoUrl = attribs.youtubeLink;
                 $scope.videoEmbed = attribs.embedSrc;
-                $scope.thumbnailSrc = attribs.thumbnailSrc;
+                $scope.thumbnailSrc = attribs.thumbnailSrc.match(/https?:\/\//) ? '' : $window.location.origin + '/' + attribs.thumbnailSrc;
                 $scope.thumbnailCaption = attribs.title;
                 $scope.thumbnailSubCaption = attribs.subtitle;
                 $scope.darkFace = attribs.darkFace;
+
+                var thumbnailElement = $element.find('.alc-video-thumbnail-image');
+                ImageFrame.updateFrame(thumbnailElement, {
+                    src: attribs.thumbnailSrc,
+                    alignImage: 'fill'
+                });
+
+                /**
+                 * No, the trusted urls have to be pre-declared in the app.js!
+                 * @param src
+                 * @returns {*}
+                 */
                 $scope.trustVideoSrc = function(src) {
-                    return $sce.trustAsResourceUrl(src + '?autoplay=1&rel=0');
+                    return $sce.trustAsResourceUrl(src);
                 };
+
             }
         };
     }
